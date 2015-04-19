f2e7c3158bc74e7f80ed4278449364905d71b8ed
Raf Van Baelen <rafvanbaelen@gmail.com>
2015-04-19 21:52:03 +0200

    Added setUp and tearDown

--- xunit.py
+++ xunit.py
@@ -2,23 +2,35 @@ class TestCase:
 	def __init__(self, name):
 		self.name = name
 		
+	def setUp(self):
+		pass
+		
 	def run(self):
+		self.setUp()
 		method = getattr(self, self.name)
 		method()
+		self.tearDown()
 
+	def tearDown(self):
+		pass
+		
 class WasRun(TestCase):
 	def __init__(self, name):
-		self.wasRun = None
 		TestCase.__init__(self, name)
 		
-	def testMethod(self):
-		self.wasRun = 1;
-
-class TestCaseTest(TestCase):
-	def testRunning(self):	
-		test = WasRun("testMethod")
-		assert(not test.wasRun)
-		test.run()
-		assert(test.wasRun)
+	def setUp(self):
+		self.log = "setUp "
 		
-TestCaseTest("testRunning").run()
+	def testMethod(self):
+		self.log = self.log + "testMethod "
+
+	def tearDown(self):
+		self.log = self.log + "tearDown "
+		
+class TestCaseTest(TestCase):
+	def testTemplateMethod(self):
+		test = WasRun("testMethod")
+		test.run()
+		assert("setUp testMethod tearDown " == test.log)
+	
+TestCaseTest("testTemplateMethod").run()
